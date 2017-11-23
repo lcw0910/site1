0f2a3c412245032cc0d5d6fd789eab771db2e767
lcw0910 <chlwn.lee@gmail.com>
2017-11-24 04:32:10 +0900

    Add DB Test

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -2,8 +2,21 @@
 
 use Faker\Generator as Faker;
 
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(App\Product::class, function (Faker $faker) {
     return [
-        //
+        'shop_idx' => function () {
+            return factory(App\Shop::class)->create()->id;
+        },
+        'product_no' => $faker->numberBetween(1, 90000),
+        'product_code' => $faker->swiftBicNumber,
+        'product_name' => $faker->text(30),
+        'description' => $faker->paragraph(4, true),
+        'price_decimal' => $faker->randomFloat(2, 1, 9000000),
+        'price_float' => $faker->randomFloat(2, 1, 9000000),
+        'price_double' => $faker->randomFloat(2, 1, 9000000),
+        'is_selling' => $faker->numberBetween(0, 1),
+        'is_display' => $faker->numberBetween(0, 1),
+        'created_at' => $faker->dateTime('now', date_default_timezone_get()),
+        'updated_at' => $faker->dateTime('now', date_default_timezone_get())
     ];
 });
